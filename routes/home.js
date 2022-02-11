a4a037ad9273f06768cd1ae05861b33ce142503c
NakyungIm <nakyoung99@gmail.com>
2022-02-12 00:14:08 +0900

    Add getHospital

--- routes/home.js
+++ routes/home.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const home = require('../controllers/home')
 const { checkToken } = require('../middlewares/auth')
 
-router.get('/hospital', checkToken, home.getDoctor)
-// router.get('/hospital', checkToken, home.getHospital)
+router.get('/doctor', checkToken, home.getDoctor)
+router.get('/hospital', checkToken, home.getHospital)
 // router.post('/reservation', checkToken, home.createReservation)
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,8 @@ const user = require('../controllers/user')
 const { checkToken } = require('../middlewares/auth')
 
 router.get('/ping', user.ping) // 핑
-router.post('/', user.createUser)
-router.post('/login', user.loginUser)
+router.post('/signup', user.createUser)
+router.post('/signin', user.loginUser)
 router.get('/', checkToken, user.getUser)
 router.put('/', checkToken, user.editUser)
 
